6de144f711fdac11f8aa69842fd74903a8595ec0
Cyvision <cyvision13@outlook.com>
2020-09-09 20:56:28 +0430

    Handling Post Request

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 
+app.use(express.json());
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
@@ -15,15 +16,33 @@ app.post('/', (req, res) => {
   res.send('You Can post to this endpoint...');
 });
 
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour :newTour
+        }
+      });
+    }
+  );
+});
+
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status:'seccess',
-    results: tours.length ,
+    status: 'seccess',
+    results: tours.length,
     data: {
       // tours:tours
-      tours
-    }
-  })
+      tours,
+    },
+  });
 });
 
 const port = 313;
