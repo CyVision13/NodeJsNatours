379a4a2c4e117381dd69285d0429d11e7cba2f48
Cyvision <cyvision13@outlook.com>
2020-09-10 13:31:25 +0430

    Respondig to URL Parameters

--- app.js
+++ app.js
@@ -45,6 +45,28 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id)
+
+  // if(id > tours.length){
+  if(!tour){
+    return res.status(404).json({
+      status: 'fail',
+      message:'Invalid ID'
+    })
+  }
+
+  
+  res.status(200).json({
+    status: 'seccess',
+    data:{
+      tour
+    }
+  });
+});
+
 const port = 313;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
