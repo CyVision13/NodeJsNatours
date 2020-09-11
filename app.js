274f4318e1310757d3936b2ea4d9cbfdd28d80d2
Cyvision <cyvision13@outlook.com>
2020-09-11 21:44:52 +0430

    Handling Delete Request

--- app.js
+++ app.js
@@ -79,6 +79,18 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data:null
+  });
+});
 
 const port = 313;
 app.listen(port, () => {
