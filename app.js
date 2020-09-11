4d8e89bf7669ee85b5bfde78711c9aceac5f9496
Cyvision <cyvision13@outlook.com>
2020-09-12 04:24:49 +0430

    Creating Our Own Middleware

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const fs = require('fs');
 const app = express();
 
 app.use(express.json());
+
+app.use((req,res,next)=>{
+  console.log('Hello from the middleware');
+  next();
+})
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
