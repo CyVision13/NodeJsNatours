dc787bc01e4636fe13e68cd86bedc1d1a6f5a701
Cyvision <cyvision13@outlook.com>
2020-09-14 19:05:46 +0430

    Serving Static Files

--- app.js
+++ app.js
@@ -2,18 +2,18 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 app.use(express.json());
 
 app.use(morgan('dev'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
   next();
 });
-
 
 // app.get('/', (req, res) => {
 //   res
@@ -25,16 +25,11 @@ app.use((req, res, next) => {
 //   res.send('You Can post to this endpoint...');
 // });
 
-
-
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-// ROUTES 
+// ROUTES
 
-
-
-
-  app.use('/api/v1/tours',tourRouter);
-  app.use('/api/v1/users',userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
