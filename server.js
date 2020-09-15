aef690cc2ed5c79f7da2fd73516010a370f3e71c
Cyvision <cyvision13@outlook.com>
2020-09-16 00:02:30 +0430

    Setting up Esling & Prettier in Vs Code (Failed to enable)

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const dotenv = require('dotenv')
-dotenv.config({path:'./config.env'})
-const app = require('./app')
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 console.log(process.env);
-
 
 const port = process.env.PORT || 313;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
+const x = 22;
+x = 23;
