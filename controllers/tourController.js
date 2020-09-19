4adcc99f3e3fa8768646364184511f40c168a441
Cyvision <cyvision13@outlook.com>
2020-09-19 20:52:54 +0430

    Making The Api Better Filtering(half)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,73 +1,81 @@
 // const fs = require('fs')
-const Tour = require('./../models/tourModel')
+const Tour = require('./../models/tourModel');
 
-
-
-
-
-exports.getTour =async (req, res) => {
+exports.getTour = async (req, res) => {
   try {
-    const id = req.params.id ;
+    const id = req.params.id;
     // const id = req.params.id * 1;
-    const tour = await Tour.findById(id)
+    const tour = await Tour.findById(id);
     // Tour.findOne({_id:req.params.id})
     res.status(201).json({
       status: 'success',
       data: {
-        tour
+        tour,
       },
     });
-  } catch(err){
+  } catch (err) {
     res.status(404).json({
-      status:'fail',
-      message:err
-    })
+      status: 'fail',
+      message: err,
+    });
   }
-  
-
-  
 };
-exports.createTour = async(req, res) => {
-  try{
+exports.createTour = async (req, res) => {
+  try {
     // const newTours = new Tour({})
-  // newTours.save()
+    // newTours.save()
 
-   const newTour = await Tour.create(req.body)
+    const newTour = await Tour.create(req.body);
 
-   res.status(201).json({
-     status: 'success',
-     data: {
-       tour: newTour,
-     },
-   });
-
-  } catch (err){
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
     res.status(400).json({
-      status:"fail",
-      message:err
-    })
+      status: 'fail',
+      message: err,
+    });
   }
-  
 };
 
 // app.post('/api/v1/tours', createTour);
 
-exports.getAllTours = async(req, res) => {
-  try{
-    const tours = await Tour.find();
-  res.status(200).json({
-    status: 'seccess',
-    results: tours.length,
-    data: {
-      // tours:tours
-      tours,
-    },
-  });
-  }catch(err){
+exports.getAllTours = async (req, res) => {
+  try {
+
+    // first way of query
+    // const tours = await Tour.find({
+    //   duration:5,
+    //   difficulty:'easy'
+    // });
+
+    // second way of query
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    //third way of query 
+    const tours = await Tour.find(req.query);
+
+
+    res.status(200).json({
+      status: 'seccess',
+      results: tours.length,
+      data: {
+        // tours:tours
+        tours,
+      },
+    });
+  } catch (err) {
     res.status(404).json({
-      status:'fail',
-      message:err
-    })
+      status: 'fail',
+      message: err,
+    });
   }
 };
 
@@ -75,46 +83,44 @@ exports.getAllTours = async(req, res) => {
 
 // app.get('/api/v1/tours/:id', getTour);
 
-exports.updateTour = async(req, res) => {
+exports.updateTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndUpdate(req.params.id,req.body,{
-      new : true,
-      runValidators:true
-    })
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
     res.status(200).json({
       status: 'success',
       data: {
-        tour
+        tour,
       },
     });
-  } catch(err){
+  } catch (err) {
     res.status(404).json({
-      status:'fail',
-      message:err
-    })
+      status: 'fail',
+      message: err,
+    });
   }
-  
 };
 
 // app.patch('/api/v1/tours/:id', updateTour);
 
-exports.deleteTour = async(req, res) => {
+exports.deleteTour = async (req, res) => {
   try {
-    const tour = await Tour.findByIdAndRemove(req.params.id,{
-      new : true,
-      runValidators:true
-    })
+    const tour = await Tour.findByIdAndRemove(req.params.id, {
+      new: true,
+      runValidators: true,
+    });
     res.status(204).json({
       status: 'success',
       data: {
-        tour :null
+        tour: null,
       },
     });
-  } catch(err){
+  } catch (err) {
     res.status(404).json({
-      status:'fail',
-      message:err
-    })
+      status: 'fail',
+      message: err,
+    });
   }
-  
 };
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
-const app = require('./app');
+
 
 dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 // const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD );
 const DB = process.env.DABABASE_LOCAL;
