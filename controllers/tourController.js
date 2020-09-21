4efe66b9bda8585b698e83faf73ff1838244bb1a
Cyvision <cyvision13@outlook.com>
2020-09-21 18:27:41 +0330

    Making the Api Better Pagination Done.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -102,14 +102,24 @@ exports.getAllTours = async (req, res) => {
 
     // 4) Pagination
     const page = req.query.page * 1 || 1 // converting to string with * 1
-    const limit = req.query.limit * 1 | 100;
+    const limit = req.query.limit * 1 || 100;
     const skip = (page - 1) * limit;
     query = query.skip(skip).limit(limit)
+
+    if(req.query.page){
+      const numTours = await Tour.countDocuments();
+ 
+      // throw err to immidiately go to Catch  Section
+      if(skip>=numTours) throw new Error('This page does not exist')
+    }
 
 
     
 // execute query
     const tours = await query
+    // query.sort().select().skip().limit()
+
+
     res.status(200).json({
       status: 'seccess',
       results: tours.length,
