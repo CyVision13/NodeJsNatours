ea492fd4b45cf25ce40761a5594c06d6c97067e3
Cyvision <cyvision13@outlook.com>
2020-09-21 19:48:18 +0330

    Refactoring API Features [filter,sort,limitFields,paginate]

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,9 +25,49 @@ class APIFeatures {
 
 
 
-    this.query.find(JSON.parse(queryStr))
+    this.query = this.query.find(JSON.parse(queryStr))
     // let query = Tour.find(JSON.parse(queryStr));
 
+    return this; // entire Object
+
+  }
+  sort(){
+    if(this.queryString.sort){
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      // console.log(sortBy);
+      this.query = this.query.sort(sortBy)
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+    return this; // entire Object
+  }
+
+  limitFields(){
+    if(this.queryString.fields){
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields)
+    } else {
+      this.query = this.query.select('-__v')
+    }
+
+    return this; // entire Object
+
+  }
+
+  paginate(){
+    const page = this.queryString.page * 1 || 1 // converting to string with * 1
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    this.query = this.query.skip(skip).limit(limit)
+
+    if(this.queryString.page){
+      const numTours = await Tour.countDocuments();
+ 
+      // throw err to immidiately go to Catch  Section
+      if(skip>=numTours) throw new Error('This page does not exist')
+    }
+
+    return this;
   }
 }
 exports.getTour = async (req, res) => {
@@ -110,42 +150,42 @@ exports.getAllTours = async (req, res) => {
     // let query = Tour.find(JSON.parse(queryStr));
 
     // 2) Sorting
-    if(req.query.sort){
-      const sortBy = req.query.sort.split(',').join(' ');
-      // console.log(sortBy);
-      query = query.sort(sortBy)
-    } else {
-      query = query.sort('-createdAt');
-    }
+    // if(req.query.sort){
+    //   const sortBy = req.query.sort.split(',').join(' ');
+    //   // console.log(sortBy);
+    //   query = query.sort(sortBy)
+    // } else {
+    //   query = query.sort('-createdAt');
+    // }
 
 
     // 3) Field limiting
-    if(req.query.fields){
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields)
-    } else {
-      query = query.select('-__v')
-    }
+    // if(req.query.fields){
+    //   const fields = req.query.fields.split(',').join(' ');
+    //   query = query.select(fields)
+    // } else {
+    //   query = query.select('-__v')
+    // }
 
 
     // 4) Pagination
-    const page = req.query.page * 1 || 1 // converting to string with * 1
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit)
+    // const page = req.query.page * 1 || 1 // converting to string with * 1
+    // const limit = req.query.limit * 1 || 100;
+    // const skip = (page - 1) * limit;
+    // query = query.skip(skip).limit(limit)
 
-    if(req.query.page){
-      const numTours = await Tour.countDocuments();
+    // if(req.query.page){
+    //   const numTours = await Tour.countDocuments();
  
-      // throw err to immidiately go to Catch  Section
-      if(skip>=numTours) throw new Error('This page does not exist')
-    }
+    //   // throw err to immidiately go to Catch  Section
+    //   if(skip>=numTours) throw new Error('This page does not exist')
+    // }
 
 
     
 // execute query
 
-    const features = new APIFeatures(Tour.find(),req.query).filter();
+    const features = new APIFeatures(Tour.find(),req.query).filter().sort();
 
     const tours = await features.query
     // query.sort().select().skip().limit()
