0c8ddbeec59e5da231b85b47c0899c6fd477ba42
Cyvision <cyvision13@outlook.com>
2020-09-23 18:59:43 +0330

    Data validation Custom  Validators 100%

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -8,7 +9,8 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trim:true,
     maxlength:[40,'A tour name must have les or equal 40 characters'],
-    minlength:[10,'A tour name must have les or equal 10 characters']
+    minlength:[10,'A tour name must have les or equal 10 characters'],
+    // validate : [validator.isAlpha , 'Tour name must only contain characters. ']
   },
   slug: {
     type: String,
