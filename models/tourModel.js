bad6c5e6c2707e21395ec5d463238a51fbbb1a2d
Cyvision <cyvision13@outlook.com>
2020-09-23 15:54:18 +0330

    Aggregation  Middleware 100%

--- models/tourModel.js
+++ models/tourModel.js
@@ -124,6 +124,15 @@ tourSchema.pre('save',function (next){
   next();
 })
 
+// AGGREGATION MIDDLEWARE 
+tourSchema.pre('aggregate',function(next){
+  
+  this.pipeline().unshift({ $match : {secretTour : {$ne : true }}})
+
+  console.log(this);
+  next();
+})
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
