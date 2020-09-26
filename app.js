b563c76c9e64e56b6a36c3bc3d8db8317541fdca
Cyvision <cyvision13@outlook.com>
2020-09-26 10:01:17 +0330

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware');
+//   next();
+// });
 
 // app.get('/', (req, res) => {
 //   res
@@ -33,5 +33,12 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req,res,next)=>{
+  res.status(404).json({
+    status:'fail',
+    message:`Cant find ${req.originalUrl} on this server!`
+  })
+})
 
 module.exports = app;
