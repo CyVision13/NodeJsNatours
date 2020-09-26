1e70fc50de72e06326dde210141cb7dd07fc1b15
Cyvision <cyvision13@outlook.com>
2020-09-26 12:01:00 +0330

    Implementing a Global Err Handling Middleware

--- app.js
+++ app.js
@@ -29,17 +29,34 @@ app.use(express.static(`${__dirname}/public`));
 
 // app.delete('/api/v1/tours/:id', deleteTour);
 
-// ROUTES
+// 3) ROUTES
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req,res,next)=>{
-  res.status(404).json({
-    status:'fail',
-    message:`Cant find ${req.originalUrl} on this server!`
+  // res.status(404).json({
+  //   status:'fail',
+  //   message:`Cant find ${req.originalUrl} on this server!`
+  // })
+  const err = new Error(`Cant find ${req.originalUrl} on this server!`)
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err); // if next get parameters express knows that we have an err
+})
+
+
+app.use((err,req,res,next)=>{ // express knows that middleware with 4 params is Err handling middleware!
+  err.statusCode = err.statusCode || 500 ;
+  err.status = err.status || 'error'
+
+  res.status(err.statusCode).json({
+    status : err.status,
+    message: err.message
   })
 })
+
 
 // An Overview of Err Handling
 
