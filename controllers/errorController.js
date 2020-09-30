d3fa0ec6d5ff032b8ace84a1ff9bbc03f958d1ee
Cyvision <cyvision13@outlook.com>
2020-09-30 09:57:52 +0330

    Handling Duplicate Database Fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -4,6 +4,13 @@ const handleCastErrorDB = err=>{
   return new AppError(message,400)
 }
 
+const handleDuplicateFieldsDB = err =>{
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`
+  return new AppError(message,400)
+}
+
 const sendErrorDev = (err , res)=>{
   res.status(err.statusCode).json({
     status: err.status,
@@ -51,7 +58,10 @@ module.exports = (err, req, res, next) => {
 
       let error = {...err}
 
-      if(error.name ==='CastError') error =handleCastErrorDB(error)
+      if(error.name ==='CastError') error =handleCastErrorDB(error);
+      if(err.code === 11000) error = handleDuplicateFieldsDB(error)
+
+
       sendErrorProd(error,res)
       
     } 
