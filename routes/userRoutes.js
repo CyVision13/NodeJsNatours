c6ab69bd71e2d119842e311511712a22336516dc
Cyvision <cyvision13@outlook.com>
2020-10-02 12:45:33 +0330

    Creating New Users

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,16 +1,20 @@
 const express = require('express');
-const userController= require('./../controllers/userController')
 
+const userController = require('./../controllers/userController');
+const authController = require('./../controllers/auth');
 
-  const router = express.Router();
+const router = express.Router();
 
+router.post('/signup', authController.signup);
 
-  router.route('/').get(userController.getAllUsers).post(userController.createUser);
-  router
-    .route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
-
-    module.exports = router;
+module.exports = router;
