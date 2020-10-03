7403f28dac1d690bb036bee0cf454936308eb953
Cyvision <cyvision13@outlook.com>
2020-10-03 12:54:20 +0330

    Signing up Users 50%

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,18 @@
 const User = require('./../models/userModel')
+const jwt = require('jsonwebtoken')
 const catchAsync = require('./../utils/catchAsync')
 exports.signup = catchAsync(async ( req,res,next)=>{
-    const newUser = await User.create(req.body)
+    // const newUser = await User.create(req.body) it had security issue
+    // .create method with req.body will craete admin user in db and we dont need this
+    // cz of it we need to let user fill the parameters that we need and use code like below
+    const newUser = await User.create({
+        name:req.body.name ,
+        email:req.body.email,
+        password: req.body.password,
+        passwordConfirm:req.body.passwordConfirm
+    });
+
+   
 
     res.status(201).json({
         status:'Success',
