1d2ab4928ddc403f220151b44d2a7f73caab2062
Cyvision <cyvision13@outlook.com>
2020-10-04 11:12:48 +0330

    Protecting Tour Routes Part 2 40%

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,82 +1,72 @@
-const e = require('express')
-const AppError = require('./../utils/appError')
-const handleCastErrorDB = err=>{
-  const message = `Invalid ${err.path} : ${err.value}.`
-  return new AppError(message,400)
-}
+const e = require('express');
+const AppError = require('./../utils/appError');
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path} : ${err.value}.`;
+  return new AppError(message, 400);
+};
 
-const handleDuplicateFieldsDB = err =>{
-
-  
-
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0]
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
   console.log(value);
-  const message = `Duplicate field value: ${value}. Please use another value!`
-  return new AppError(message,400)
-}
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
 
-const handleValidationErrorDB = err =>{
-
-  const errors = Object.values(err.errors).map(el => el.message)
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
 
   const message = `Invalid input data. ${errors.join('. ')}`;
-  return new AppError(meesage,400)
-}
+  return new AppError(meesage, 400);
+};
 
-
-const sendErrorDev = (err , res)=>{
+const handleJWTError = (err) =>
+  new AppError('Invalid token. Please log in again!', 401);
+const handleJWTExpiredError = (err) =>
+  new AppError('Your token is expired!please log in again', 401);
+const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
-    error:err.status,
+    error: err.status,
     message: err.message,
-    stack:err.stack
+    stack: err.stack,
   });
-}
+};
 
-const sendErrorProd = (err,res)=>{ 
-  
-  if(err.isOperational) {
-    res.status(err.statusCode).json({ 
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
-      
     });
   } else {
-    // 1) Log error 
-    // console.error('ERROR ',err); 
+    // 1) Log error
+    // console.error('ERROR ',err);
 
     // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something went very wrong!'
-    })
+      message: 'Something went very wrong!',
+    });
   }
-
-  
-
-}
+};
 
 module.exports = (err, req, res, next) => {
-    // express knows that middleware with 4 params is Err handling middleware!
-    // console.log(err.stack);
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+  // express knows that middleware with 4 params is Err handling middleware!
+  // console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-    if(process.env.Node_ENV ==='development'){
+  if (process.env.Node_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.Node_ENV === 'production') {
+    let error = { ...err };
 
-      sendErrorDev(err,res)
-   
-
-    } else if (process.env.Node_ENV ==='production'){
-
-      let error = {...err}
-
-      if(error.name ==='CastError') error =handleCastErrorDB(error);
-      if(err.code === 11000) error = handleDuplicateFieldsDB(error)
-      if(error.name ==='ValidationError') error = handleValidationErrorDB(error)
-
-      sendErrorProd(error,res)
-      
-    } 
-  
-  }  
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+    if (err.name === 'JsonWebTokenError') error = handleJWTError(error);
+    if (err.name === 'TokenExpiredError') error = handleJWTExpiredError(error);
+    sendErrorProd(error, res);
+  }
+};
