54eb4dc6a4d27cfd1f1233e0c6ac65985e306505
Cyvision <cyvision13@outlook.com>
2020-10-11 11:09:41 +0330

    deleting the Current User

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,6 +47,17 @@ exports.updateUser = (req, res) => {
     message: 'This route is not yet definded',
   });
 };
+
+exports.deleteMe = catchAsync(async (req,res,next)=>{
+  await User.findByIdAndUpdate(req.user.id, {active: false})
+
+
+  res.status(204).json({
+    status:'success',
+    data:null
+  })
+})
+
 exports.deleteUser = (req, res) => {
   res.status(500).json({
     status: 'error',
--- models/userModel.js
+++ models/userModel.js
@@ -48,6 +48,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active:{
+    type: Boolean,
+    default: true,
+    select:false
+  }
   // Confirm password must be the same as password
 });
 
@@ -69,6 +74,12 @@ userSchema.pre('save',function(next){
   this.passworChagedAt = Date.now() - 1000; // for make sure server's delay get cought
   next();
 })
+
+userSchema.pre(/^find/,function(next){
+  // this points to the current query
+  this.find({active:{$ne:false}});
+  next();
+});
 
 userSchema.methods.correctPassword = async function (
   candidatePassword,
