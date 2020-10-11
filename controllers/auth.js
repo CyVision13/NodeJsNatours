17e34e3d4f3fa402b033d829e0c5819f88de1bde
Cyvision <cyvision13@outlook.com>
2020-10-11 12:52:28 +0330

    Sending JWT via Cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -14,6 +14,21 @@ const signToken = (id) => {
 
 const createSendToken = (user,statusCode,res)=>{
   const token = signToken(user._id)
+  const cookieOption = {
+    
+      expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN *24*60*60*1000),
+      // secure:true,
+      httpOnly: true
+    
+  }
+  if(process.env.NODE_ENV ==='production') cookieOption.secure = true
+
+  res.cookie('jwt',token,cookieOption)
+
+  
+  // remove the password from the output
+  user.password = undefined
+
   res.status(statusCode).json({
     status: 'Success',
     token,
