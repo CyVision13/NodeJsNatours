b87f0a6d539a36880a374b76380516ce0a34303f
Cyvision <cyvision13@outlook.com>
2020-10-13 11:02:11 +0330

    Modelling Reviews Parent Refrencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+
 // const User = require('./userModel')
 
 const tourSchema = new mongoose.Schema({
