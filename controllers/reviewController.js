331822d28f0f8a4f516aea5309a10f70cf6615c9
Cyvision <cyvision13@outlook.com>
2020-10-14 09:59:21 +0330

    Adding a Nested GET Endpoint

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -9,9 +9,10 @@ const AppError = require('../utils/appError');
 
 
 exports.getAllReviews = catchAsync(async (req,res,next)=>{
-    
+  let filter = {}
+    if(req.params.tourId) filter = {tour:req.params.tourId}
 
-    const reviews = await Review.find();
+    const reviews = await Review.find(filter);
 
 
       res.status(201).json({
