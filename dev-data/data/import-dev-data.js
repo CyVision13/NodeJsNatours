9e20bc575cdc0c1e94a6044287107db57a1a8cf0
Cyvision <cyvision13@outlook.com>
2020-10-15 09:15:01 +0330

    Importing Review and User Data

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel')
+const Review = require('./../../models/reviewModel')
+const User = require('./../../models/userModel')
 
 
 dotenv.config({ path: './config.env' });
@@ -18,11 +20,15 @@ mongoose.connect(DB,{
 }).then(()=> console.log('DB connection succesfull'));
 //read json file
 const tours =JSON.parse( fs.readFileSync(`${__dirname}/tours.json`,'utf-8'))
+const users =JSON.parse( fs.readFileSync(`${__dirname}/users.json`,'utf-8'))
+const reviews =JSON.parse( fs.readFileSync(`${__dirname}/reviews.json`,'utf-8'))
 
 //import data into db
 const importData = async()=>{
     try{
         await Tour.create(tours)
+        await User.create(users, {validateBeforeSave:false})
+        await Review.create(reviews)
         console.log('Data successfully loaded!');
         process.exit();
     }catch (err){
@@ -35,6 +41,8 @@ const importData = async()=>{
 const deleteData = async ()=>{
     try{
         await Tour.deleteMany()
+        await User.deleteMany()
+        await Review.deleteMany()
         console.log('Data successfully deleted!');
         process.exit()
     }catch (err){
