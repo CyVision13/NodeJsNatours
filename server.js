24f5d2d6239f023d52f8c6c2491d73a37a538cc0
cyvision13 <cyvision13@outlook.com>
2020-11-28 05:15:41 -0500

    Use MongoDb Atlass

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-// const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD );
-const DB = process.env.DABABASE_LOCAL;
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.DATABASE_PASSWORD );
+// const DB = process.env.DABABASE_LOCAL;
 
 
 
